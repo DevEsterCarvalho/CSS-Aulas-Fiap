dc426839bcf71a3fe707c05638798c855fbf9850
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-08-03 16:29:25 -0300

    subindo exercícios

---
+++ JS/consoleLog.js
@@ -0,0 +1,4 @@
+//Código Javascript
+console.log('Olá eu sou o console.');
+console.log('Aqui você poderá verificar o que está
+             acontecendo em sua página.');
---
+++ JS/consoleNode.js
@@ -0,0 +1,2 @@
+console.log("Usando Node e o console");
+console.log("Essas mensagens aparecerão no Node e no console");
