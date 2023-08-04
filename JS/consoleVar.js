5787aab4a800f5a2f0bd9a2c7a541db0082b6e45
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-08-04 16:51:26 -0300

    subindo exercícios

---
+++ JS/consoleVar.js
@@ -0,0 +1,11 @@
+//declarando as variáveis
+let userId = 4567;
+let userName = 'usuarioJavascript';
+let userLogged = true;
+let userEmail;
+
+//exibindo no console
+console.log(userId);
+console.log(userName);
+console.log(userLogged);
+console.log(userEmail);
