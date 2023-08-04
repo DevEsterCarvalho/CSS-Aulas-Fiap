aaaab10d6dabdcacbdd6529654a9e46e4334bdd7
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-08-04 19:15:13 -0300

    subindo exercícios

---
+++ JS/consoleConstante.js
@@ -0,0 +1,11 @@
+//declarando as constantes
+const DATA_NASCIMENTO = '01/01/1995';
+const CPF = '252.433.368-04';
+const RG_SP = '29.365.924-6';
+
+//exibindo no console
+console.log(DATA_NASCIMENTO);
+console.log(CPF);
+console.log(RG_SP);
+
+//constantes são como as váriaveis mas os seus valores são fixos, não podem ser mudados
